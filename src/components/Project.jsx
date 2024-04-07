944a4f76eb7cbedd04aebc1420ac969d849c6cd8
Sanjana M <sanjanamurugesan06@gmail.com>
2024-04-07 23:11:03 +0530

    .

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,24 +1,158 @@
 import React from "react";
 
-function Project(){
-    return(
-        <div>
-            <hr  id="project" />
-            <section>
-                <div className="container" style={{"display": "block"}}>
-                    <h3 className="align-center" >My Projects</h3>
-                    <h4 style={{"margin-left": "10px" ,"color": "#D0B214"}}>Paytm</h4>
-                    <p className="para font">In this project the three unique pages of paytm has been designed using figma and code for the same has been implemented. The index page is contact page followed by About page and Investor Relation page.</p>
-                <br/><div style={{"margin-left": "10px"}}>
-                    <span><a href="https://github.com/Sanjana11062004/paytm-clone"><img width="40px" height="40px" src="/images/github (2).png" alt="github" /></a></span>
-                    <span style={{"margin-left": "1%"}}><a href="https://www.figma.com/file/oVAqHZG293yoP2mo5JQ1P2/paytm?type=design&node-id=0-1&t=kLOZXZFu21JArmLk-0"><img width="40px" height="40px" src="/images/figma.png" alt="figma" /></a></span>
-                    <span style={{"margin-left": "1%"}}><a href="https://paytm-clone-2beq.vercel.app/"><img width="40px" height="40px" src="/images/microsoft (2).png" alt="chrome" /></a></span>
-                </div>
-                </div><br/>
-                <br/>
-            </section><hr></hr>
+function Project() {
+  return (
+    <div>
+      <hr id="project" />
+      <section>
+        <div className="container" style={{ display: "block" }}>
+          <h3 className="align-center">My Projects</h3>
+          <h4 style={{ "margin-left": "10px", color: "#D0B214" }}>Paytm</h4>
+          <p className="para font">
+            In this project the three unique pages of paytm has been designed
+            using figma and code for the same has been implemented. The index
+            page is contact page followed by About page and Investor Relation
+            page.
+          </p>
+          <br />
+          <div style={{ "margin-left": "10px" }}>
+            <span>
+              <a href="https://github.com/Sanjana11062004/paytm-clone">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/github (2).png"
+                  alt="github"
+                />
+              </a>
+            </span>
+            <span style={{ "margin-left": "1%" }}>
+              <a href="https://www.figma.com/file/oVAqHZG293yoP2mo5JQ1P2/paytm?type=design&node-id=0-1&t=kLOZXZFu21JArmLk-0">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/figma.png"
+                  alt="figma"
+                />
+              </a>
+            </span>
+            <span style={{ "margin-left": "1%" }}>
+              <a href="https://paytm-clone-2beq.vercel.app/">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/microsoft (2).png"
+                  alt="chrome"
+                />
+              </a>
+            </span>
+          </div>
         </div>
-    )
+        <div className="container" style={{ display: "block" }}>
+          <h4 style={{ "margin-left": "10px", color: "#D0B214" }}>Grocery</h4>
+          <p className="para font">
+            Grocery is a convenient web application designed to simplify your
+            shopping experience. With Grocery, you can easily create and manage
+            your grocery shopping lists anytime, anywhere. Say goodbye to
+            forgetting items or having to remember everything you need to buy.
+          </p>
+          <br />
+          <div style={{ "margin-left": "10px" }}>
+            <span>
+              <a href="https://github.com/Sanjana11062004/Grocery">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/github (2).png"
+                  alt="github"
+                />
+              </a>
+            </span>
+            
+            <span style={{ "margin-left": "1%" }}>
+              <a href="https://grocery-bcnw.vercel.app/">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/microsoft (2).png"
+                  alt="chrome"
+                />
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className="container" style={{ display: "block" }}>
+          <h4 style={{ "margin-left": "10px", color: "#D0B214" }}>
+            Task Manager
+          </h4>
+          <p className="para font">
+            I developed a task manager application that allows users to
+            efficiently manage their tasks by providing functionality to add new
+            tasks and remove existing ones.
+          </p>
+          <br />
+          <div style={{ "margin-left": "10px" }}>
+            <span>
+              <a href="https://github.com/Sanjana11062004/To-do-list">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/github (2).png"
+                  alt="github"
+                />
+              </a>
+            </span>
+            <span style={{ "margin-left": "1%" }}>
+              <a href="https://to-do-list-bay-six.vercel.app/">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/microsoft (2).png"
+                  alt="chrome"
+                />
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className="container" style={{ display: "block" }}>
+          <h4 style={{ "margin-left": "10px", color: "#D0B214" }}>
+            Swiggy-Clone
+          </h4>
+          <p className="para font">
+            I developed a dynamic web application inspired by Swiggy. The
+            application features functionalities such as viewing menus, adding
+            items to the cart, and calculating shipping costs dynamically.
+          </p>
+          <br />
+          <div style={{ "margin-left": "10px" }}>
+            <span>
+              <a href="https://github.com/Sanjana11062004/Swiggy_Clone">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/github (2).png"
+                  alt="github"
+                />
+              </a>
+            </span>
+            <span style={{ "margin-left": "1%" }}>
+              <a href="https://swiggy-clone-lac.vercel.app/">
+                <img
+                  width="40px"
+                  height="40px"
+                  src="/images/microsoft (2).png"
+                  alt="chrome"
+                />
+              </a>
+            </span>
+          </div>
+        </div>
+        <br />
+        <br />
+      </section>
+      <hr></hr>
+    </div>
+  );
 }
 
 export default Project;
--- src/components/TechnicalSkills.jsx
+++ src/components/TechnicalSkills.jsx
@@ -1,47 +1,57 @@
 import React from "react";
 
-function TechnicalSkills(){
-    return(
-        <div>
-            <hr></hr>
-            <section>
-                <div className="container">
-                    <h3 className="align-center">Technical Skills</h3>
-                </div>
-                <div className="ALIGN-ITEMS" >
-                    <p>HTML</p>
-                    <div className="container1">
-                        <div className="skills html">90%</div>
-                    </div>
-
-                    <p>CSS</p>
-                    <div className="container1">
-                    <div className="skills css">80%</div>
-                    </div>
-
-                    <p>JavaScript</p>
-                    <div className="container1">
-                    <div className="skills js">50%</div>
-                    </div>
-
-                    <p>C</p>
-                    <div className="container1">
-                    <div className="skills c">70%</div>
-                    </div>
-
-                    <p>Python</p>
-                    <div className="container1">
-                    <div className="skills py">50%</div>
-                    </div>
-
-                    <p>Java</p>
-                    <div className="container1">
-                    <div className="skills java">80%</div>
-                    </div>
-                </div>
-            </section>
+function TechnicalSkills() {
+  return (
+    <div>
+      <hr></hr>
+      <section>
+        <div className="container">
+          <h3 className="align-center">Technical Skills</h3>
         </div>
-    )
+        <div className="ALIGN-ITEMS">
+          <p>HTML</p>
+          <div className="container1">
+            <div className="skills html">90%</div>
+          </div>
+
+          <p>CSS</p>
+          <div className="container1">
+            <div className="skills css">80%</div>
+          </div>
+
+          <p>JavaScript</p>
+          <div className="container1">
+            <div className="skills java">80%</div>
+          </div>
+
+          <p>ReactJS</p>
+          <div className="container1">
+            <div className="skills java blu">80%</div>
+          </div>
+
+          <p>Java</p>
+          <div className="container1">
+            <div className="skills java green">80%</div>
+          </div>
+
+          <p>MySQL</p>
+          <div className="container1">
+            <div className="skills c red">70%</div>
+          </div>
+
+          <p>C</p>
+          <div className="container1">
+            <div className="skills c blu">70%</div>
+          </div>
+
+          <p>Python</p>
+          <div className="container1">
+            <div className="skills py green">50%</div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default TechnicalSkills;
